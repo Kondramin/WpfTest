3b0dca8f58722fd0d34fda5e8bcd715b773eaa89
Kondratyev, Artyom <a3.kondratyev@itransition.com>
2022-04-14 15:58:36 +0300

    Added helpful class to get strings from appsettings.json

---
+++ WpfTest/Services/ConfigApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTest.Services
+{
+    public class ConfigApp
+    {
+        public static string Token { get; set; }
+        public static string StockUri { get; set; }
+        public static string SomeValues { get; set; }
+
+
+    }
+}
