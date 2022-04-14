b40b4344632bc6c2453be9222c5401e536571dea
Kondratyev, Artyom <a3.kondratyev@itransition.com>
2022-04-14 16:01:30 +0300

    Added necessary services

--- WpfTest/App.xaml.cs
+++ WpfTest/App.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using WpfTest.Interfaces;
+using WpfTest.Services;
+using WpfTest.ViewModels;
 
 namespace WpfTest
 {
@@ -13,5 +14,69 @@ namespace WpfTest
     /// </summary>
     public partial class App : Application
     {
+        //public static Window ActiveWindow => Application.Current.Windows
+        //    .OfType<Window>()
+        //    .FirstOrDefault(w => w.IsActive);
+
+
+        //public static Window FocusedWindow => Application.Current.Windows
+        //    .OfType<Window>()
+        //    .FirstOrDefault(w => w.IsFocused);
+
+
+        //public static Window CurrentWindow => FocusedWindow ?? ActiveWindow;
+
+
+        public static bool IsDesignTime { get; private set; } = true;
+
+
+        private static IHost __Host;
+
+        public static IHost Host => __Host
+            ??= Program.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+
+        
+
+        public static IServiceProvider Services => Host.Services;
+
+
+        internal static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            ConfigApp.Token = host.Configuration["Token"];
+            ConfigApp.SomeValues = host.Configuration["SomeValues"];
+            
+            services.AddViewModels();
+            services.AddHttpClient<StockdataClient>(client => client.BaseAddress = new Uri(host.Configuration["StockUri"]));
+
+            services.AddTransient<IStringConverter, StringConverter>();
+        }
+        
+
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            IsDesignTime = false;
+            var host = Host;
+
+
+
+            using (var scope = Services.CreateScope())
+            {
+                //await scope.ServiceProvider.GetRequiredService<DbInitializer>().InitializeAsync();
+                //Configuration.Bind("Project", new CongigApp());
+            }
+
+
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
